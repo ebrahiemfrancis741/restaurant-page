cf0d2d91425f1e8bf90483743334ccd08b5bfd60
Ebrahiem Francis <ebrahiemfrancis741@gmail.com>
2025-01-01 21:55:35 +0200

    Add click event handlers for the navigation buttons

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 import "./styles.css";
 import { loadHome } from "./home.js"
 import { loadMenu } from "./menu.js";
+import { loadContact } from "./contact.js";
 
-// loadHome();
-loadMenu();
+loadHome();
+// loadMenu();
+
+(function setupEventListeners(){
+  let contentContainer = document.querySelector("#content");
+
+  let homeBtn = document.querySelector("#homeBtn");
+  homeBtn.addEventListener("click", ()=>{
+    contentContainer.replaceChildren();
+    loadHome();
+  });
+
+  let menuBtn = document.querySelector("#menuBtn");
+  menuBtn.addEventListener("click", ()=>{
+    contentContainer.replaceChildren();
+    loadMenu();
+  });
+
+  let contactBtn = document.querySelector("#contactBtn");
+  contactBtn.addEventListener("click", ()=>{
+    contentContainer.replaceChildren();
+    loadContact();
+  });
+
+})();
