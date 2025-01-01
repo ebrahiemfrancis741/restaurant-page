c999165579076aac12022557bab87669eb04d6c4
Ebrahiem Francis <ebrahiemfrancis741@gmail.com>
2025-01-01 06:14:49 +0200

    Add object array that holds menu information

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import "./styles.css";
 import { loadHome } from "./home.js"
 
-loadHome();
+// loadHome();
---
+++ src/menu.js
@@ -0,0 +1,87 @@
+
+let menu  = [
+  // signature dishes
+  {
+    category: "Signature Dished",
+    menuItems: [
+      {
+        name: "Golden Beet Tartare",
+        description: 
+        `
+        Thinly sliced golden beets paired with goat cheese, avocado, 
+        and a touch of lemon zest, drizzled with a balsamic reduction.
+        `
+      },
+      {
+        name: "Willow-Smoked Duck Breast",
+        description: 
+        `
+        Tender duck breast smoked with willow branches, served with a cherry compote, 
+        roasted root vegetables, and a rich, creamy parsnip puree.
+        `
+      },
+      {
+        name: "Crispy Duck Fat Fries",
+        description: 
+        `
+        Golden fries seasoned with rosemary and sea salt, served with a house-made aioli.
+        `
+      },
+      {
+        name: "Seared Scallops with Lemon-Ginger Butter",
+        description: 
+        `
+        Fresh scallops seared to perfection, served with sautéed spinach and a tangy 
+        lemon-ginger butter sauce.
+        `
+      }
+    ]
+  },
+  // drinks
+  {
+    category: "Drinks",
+    menuItems: [
+      {
+        name: "The Willow's Embrace",
+        description: 
+        `
+        A signature cocktail made with lavender-infused gin, elderflower liqueur, 
+        and a hint of citrus, served with a sprig of fresh rosemary.
+        `
+      },
+      {
+        name: "Amber Nights",
+        description: 
+        `
+        A rich bourbon-based cocktail with a touch of smoked maple syrup and a dash of 
+        bitters, garnished with an orange peel twist.
+        `
+      }
+    ]
+  },
+  // deserts
+  {
+    category: "Dessert",
+    menuItems: [
+      {
+        name: "Lavender Honey Cheesecake",
+        description: 
+        `
+       A delicate, velvety cheesecake made with local honey and a hint of lavender, 
+       served with fresh berries and a drizzle of honey.
+        `
+      },
+      {
+        name: "Willow Tree Chocolate Mousse",
+        description: 
+        `
+        Decadent dark chocolate mousse topped with whipped cream and edible gold leaf.
+        `
+      }
+    ]
+  }
+]
+
+export function loadMenu(){
+
+}
